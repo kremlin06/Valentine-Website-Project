3a0f9b95de9b0d50c9e4e7eda84b708e65d74657
Kremlin <jhonkenth1307@gmail.com>
2026-02-08 18:04:24 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import './index.css'
-import './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
